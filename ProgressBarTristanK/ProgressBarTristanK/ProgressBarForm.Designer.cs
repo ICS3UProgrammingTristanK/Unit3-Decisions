6acc2dfaaf84829a2c24d9efcd3699d010b6beb3
Tristan Kalabric <32547188+TristanKalabric@users.noreply.github.com>
2018-10-22 10:13:29 -0400

    Created a Progress Bar

---
+++ ProgressBarTristanK/ProgressBarTristanK/ProgressBarForm.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace ProgressBarTristanK
+{
+    partial class frmProgressBar
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.prbLoading = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.Location = new System.Drawing.Point(80, 60);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(104, 40);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // prbLoading
+            // 
+            this.prbLoading.Location = new System.Drawing.Point(44, 129);
+            this.prbLoading.Name = "prbLoading";
+            this.prbLoading.Size = new System.Drawing.Size(172, 41);
+            this.prbLoading.TabIndex = 1;
+            // 
+            // frmProgressBar
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.prbLoading);
+            this.Controls.Add(this.btnStart);
+            this.Name = "frmProgressBar";
+            this.Text = "Progress Bar";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.ProgressBar prbLoading;
+    }
+}
+
---
+++ ProgressBarTristanK/ProgressBarTristanK/ProgressBarForm.cs
@@ -0,0 +1,42 @@
+﻿/*
+ * Created By: Tristan Kalabric
+ * Created On: October 22, 2018
+ * Created For ICS3U Programming
+ * Daily Assignment - Day #20 - Progress Bar
+ * This program displays a progress bar that loads when the user clicks start
+ */
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProgressBarTristanK
+{
+    public partial class frmProgressBar : Form
+    {
+        public frmProgressBar()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            //declare variables and constants
+            const int MAX_PROGRESS_BAR_VALUE = 100;
+            int valueOfProgressBar = 0;
+
+            //Increment the value of the progress bar by 5 ach time until it has reached its max value
+            do
+            {
+                this.prbLoading.Value = valueOfProgressBar;
+                valueOfProgressBar = valueOfProgressBar + 5;
+                this.Refresh();
+            } while (valueOfProgressBar <= MAX_PROGRESS_BAR_VALUE);
+        }
+    }
+}
