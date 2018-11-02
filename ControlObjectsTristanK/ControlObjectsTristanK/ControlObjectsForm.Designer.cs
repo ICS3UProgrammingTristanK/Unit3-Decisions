d69db278d05a498e2a5411cddb0c0db76ed9ae68
Tristan Kalabric <32547188+TristanKalabric@users.noreply.github.com>
2018-11-02 10:51:19 -0400

    Creted Control Objects program

---
+++ ControlObjectsTristanK/ControlObjectsTristanK/ControlObjectsForm.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace ControlObjectsTristanK
+{
+    partial class frmControlObjects
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnChangeColour = new System.Windows.Forms.Button();
+            this.lblChangeColour = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnChangeColour
+            // 
+            this.btnChangeColour.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnChangeColour.Location = new System.Drawing.Point(70, 43);
+            this.btnChangeColour.Name = "btnChangeColour";
+            this.btnChangeColour.Size = new System.Drawing.Size(181, 40);
+            this.btnChangeColour.TabIndex = 0;
+            this.btnChangeColour.Text = "Change Colour";
+            this.btnChangeColour.UseVisualStyleBackColor = true;
+            this.btnChangeColour.Click += new System.EventHandler(this.btnChangeColour_Click);
+            // 
+            // lblChangeColour
+            // 
+            this.lblChangeColour.AutoSize = true;
+            this.lblChangeColour.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblChangeColour.Location = new System.Drawing.Point(85, 136);
+            this.lblChangeColour.Name = "lblChangeColour";
+            this.lblChangeColour.Size = new System.Drawing.Size(156, 25);
+            this.lblChangeColour.TabIndex = 1;
+            this.lblChangeColour.Text = "Change Colour";
+            this.lblChangeColour.Click += new System.EventHandler(this.lblChangeColour_Click);
+            // 
+            // frmControlObjects
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(314, 209);
+            this.Controls.Add(this.lblChangeColour);
+            this.Controls.Add(this.btnChangeColour);
+            this.Name = "frmControlObjects";
+            this.Text = "Control Objects";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnChangeColour;
+        private System.Windows.Forms.Label lblChangeColour;
+    }
+}
+
---
+++ ControlObjectsTristanK/ControlObjectsTristanK/ControlObjectsForm.cs
@@ -0,0 +1,57 @@
+﻿/*
+ * Created by: Tristan Kalabric
+ * Created on: November 2, 2018
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #27 - Control Objects
+ * This program changes the colour of the button and the label when the user clicks on them
+*/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ControlObjectsTristanK
+{
+    public partial class frmControlObjects : Form
+    {
+        public frmControlObjects()
+        {
+            InitializeComponent();
+        }
+
+        private void btnChangeColour_Click(object sender, EventArgs e)
+        {
+            //Loop through each object on the form and make it green
+            foreach (Control aControlObject in this.Controls)
+            {
+                aControlObject.BackColor = Color.Green;
+
+                //If the object is a label, make it purple
+                if (aControlObject.GetType() == typeof(Label))
+                {
+                    aControlObject.BackColor = Color.Purple;
+                }
+            }
+        }
+
+        private void lblChangeColour_Click(object sender, EventArgs e)
+        {
+            //Loop through each object on the form and make it red
+            foreach (Control aControlObject in this.Controls)
+            {
+                aControlObject.BackColor = Color.Red;
+
+                //If the object is a label, make it red
+                if (aControlObject.GetType() == typeof(Label))
+                {
+                    aControlObject.BackColor = Color.Red;
+                }
+            }
+        }
+    }
+}
